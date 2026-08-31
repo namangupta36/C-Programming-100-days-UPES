0f3b476d13ea586fa34266568c16207b990bc4f6
Naman Gupta <namangupta10350@gmail.com>
2026-09-01 03:19:29 +0530

    Q27 (Loops without Arrays/Strings)

---
+++ Day 14_Q27.c
@@ -0,0 +1,14 @@
+
+// Write a prgram to print the sum of the first n odd numbers.
+#include <stdio.h>
+int main ()
+{
+    int n, i, sum = 0;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + (2 * i - 1);
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
